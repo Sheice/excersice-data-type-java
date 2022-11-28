9be4b06e4b4ef9566281ca8bdeff9a23b01f188b
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-28 17:16:47 -0300

    data types java

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        // data types
+
+        // integer numbers
+
+        byte varByte = 1;
+        short varShort = 5;
+        int varInt = 50;
+        long varLong = 500;
+
+        // decimal numbers
+
+        float varFloat = 3.1f;
+        double varDouble = 2.3;
+
+
+        // boolean
+        boolean varBoolean = false;
+        boolean varBoolean2 = true;
+
+
+        // text
+        char varChar = 'J';
+        String varString = "HAHA HAHA";
+    }
+}
